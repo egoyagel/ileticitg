59dc6adf401ab15972f1b345a42fa572c618dc39
egoyagel <egoyagel@gmail.com>
2023-10-03 09:40:42 +0300

    Add files via upload

--- iletici.py
+++ iletici.py
@@ -13,32 +13,45 @@ async def yasakli_kelimeleri_oku():
     except FileNotFoundError:
         return []
 
+async def kaynak_sayisi_al():
+    try:
+        kaynak_sayisi = int(input("Kaç adet kaynak sohbet seçmek istersiniz: "))
+        return kaynak_sayisi
+    except ValueError:
+        print("Lütfen geçerli bir sayı girin.")
+        return await kaynak_sayisi_al()
+
+async def kaynak_sohbet_sec(client):
+    dialogs = await client.get_dialogs()
+    print("Sohbet Listesi:")
+    for i, dialog in enumerate(dialogs, 1):
+        print(f"{i}. {dialog.name}")
+
+    kaynaklar = []
+    kaynak_sayisi = await kaynak_sayisi_al()
+
+    for i in range(kaynak_sayisi):
+        kaynak_sohbet_num = int(input(f"{i + 1}. kaynak sohbeti seçin (numara ile): "))
+        kaynak_sohbet = dialogs[kaynak_sohbet_num - 1]
+        kaynaklar.append(kaynak_sohbet)
+
+    return kaynaklar
+
 async def main():
     yasakli_kelimeler = await yasakli_kelimeleri_oku()
 
     # Telethon ile oturum aç
     async with TelegramClient('session_name', api_id, api_hash) as client:
-        # Kullanıcının sohbet listesini al
         dialogs = await client.get_dialogs()
+        kaynaklar = await kaynak_sohbet_sec(client)
 
-        # Kullanıcıya sohbetleri göster
-        print("Sohbet Listesi:")
-        for i, dialog in enumerate(dialogs, 1):
-            print(f"{i}. {dialog.name}")
-
-        # Kullanıcıdan kaynak ve hedef sohbetleri seçmesini iste
-        kaynak_sohbet_num = int(input("Kaynak sohbeti seçin (numara ile): "))
         hedef_sohbet_num = int(input("Hedef sohbeti seçin (numara ile): "))
-
-        # Kaynak ve hedef sohbetleri belirle
-        kaynak_sohbet = dialogs[kaynak_sohbet_num - 1]
         hedef_sohbet = dialogs[hedef_sohbet_num - 1]
 
-        print(f"Kaynak sohbet: {kaynak_sohbet.name}")
         print(f"Hedef sohbet: {hedef_sohbet.name}")
 
         # Yeni mesajları izle
-        @client.on(events.NewMessage(chats=[kaynak_sohbet]))
+        @client.on(events.NewMessage(chats=kaynaklar))
         async def handle_new_message(event):
             mesaj = event.message
             mesaj_metni = mesaj.text.lower()  # Mesajı küçük harf olarak alın
@@ -46,10 +59,10 @@ async def main():
             # Yasaklı kelimeleri kontrol et
             for yasakli_kelime in yasakli_kelimeler:
                 if yasakli_kelime in mesaj_metni:
-                    print(f"❗Yasaklı kelime tespit edildi: {yasakli_kelime}")
+                    print(f"Yasaklı kelime tespit edildi: {yasakli_kelime} (Kaynak: {event.chat.title})")
                     return  # Yasaklı kelime bulunursa mesajı iletme
 
-            # Yasaklı kelime bulunmadıysa mesajı ilet
+            # Yasaklı kelime bulunmadıysa mesajı hedef sohbete ileterek çalışır
             await client.send_message(hedef_sohbet, mesaj)
 
         # Mesajları izlemek için başla
